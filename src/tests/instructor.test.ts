f6d78007d8f41d4adadd98cb3de8a413578e61dc
Vinicius Francisco Pinha <viniciuspinha@alunos.utfpr.edu.br>
2023-09-13 15:36:51 -0300

    test: added instructor tests

--- src/tests/instructor.test.ts
+++ src/tests/instructor.test.ts
@@ -1,4 +1,10 @@
-import { createInstructor } from '../controllers/instructor';
+import {
+  getAllInstructors,
+  getSingleInstructor,
+  createInstructor,
+  updateInstructor,
+  deleteInstructor,
+} from '../controllers/instructor';
 import { InstructorModel } from '../models/instructor';
 import { Request, Response, NextFunction } from 'express';
 
@@ -6,73 +12,210 @@ jest.mock('../models/instructor', () => ({
   InstructorModel: {
     findOne: jest.fn(),
     create: jest.fn(),
+    find: jest.fn(),
+    findOneAndUpdate: jest.fn(),
   },
 }));
 
-describe('createInstructor', () => {
+describe('Instructors tests', () => {
   beforeEach(() => {
     // Limpa todas as instâncias e chamadas para o mock
     (InstructorModel.findOne as jest.Mock).mockClear();
     (InstructorModel.create as jest.Mock).mockClear();
+    (InstructorModel.find as jest.Mock).mockClear();
+    (InstructorModel.findOneAndUpdate as jest.Mock).mockClear();
   });
 
-  it('should return "Email already exists" error if email is already in use', async () => {
-    const req = {
-      body: {
-        name: 'Vinicius',
-        email: 'vinicius2@hotmail.com',
-        password: 'secret',
-        expertise: ['Matéria3'],
-        availability: ['Sex7'],
-      },
-    };
+  afterAll(() => {
+    // Limpa todos os mocks
+    jest.clearAllMocks();
+  });
 
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Response;
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+  } as unknown as Response;
 
-    const next = jest.fn();
+  const next = jest.fn();
 
-    // Simula que um instructor com o email fornecido já existe
-    (InstructorModel.findOne as jest.Mock).mockResolvedValue({
-      email: 'viniciuspinha2@hotmail.com',
+  // Testes para o método getSingleInstructor
+  describe('getAllInstructors', () => {
+    it('should return "There is none instructors registered" error if there is no instructors registered', async () => {
+      // Simula que não há nenhum instructor registrado
+      (InstructorModel.find as jest.Mock).mockResolvedValue([]);
+
+      await getAllInstructors(
+        {} as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        msg: 'There is none instructors registered',
+      });
+    });
+  });
+
+  // Testes para o método getSingleInstructor
+  describe('getSingleInstructor', () => {
+    it('should return "The user ID is incorrect" error if the id is not a valid ObjectId', async () => {
+      const req = {
+        params: {
+          id: '123',
+        },
+      };
+
+      await getSingleInstructor(
+        req as any as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        msg: 'The user ID is incorrect',
+      });
     });
 
-    await createInstructor(
-      req as Request,
-      res as Response,
-      next as NextFunction
-    );
+    it('should return "There is no instructor with id: {id}" error if there is no instructor with the given id', async () => {
+      const req = {
+        params: {
+          id: '6501ebfc87d46e3a6861844d',
+        },
+      };
 
-    expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({ msg: 'Email already exists' });
+      // Simula que não há nenhum instructor com o id fornecido
+      (InstructorModel.findOne as jest.Mock).mockResolvedValue(null);
+
+      await getSingleInstructor(
+        req as any as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({
+        msg: `There is no instructor with id: ${req.params.id}`,
+      });
+    });
   });
 
-  it('should return "Missing fields" error if some value is empty', async () => {
-    const req = {
-      body: {
-        email: 'vinicius2@hotmail.com',
-        password: 'secret',
-        expertise: ['Matéria3'],
-        availability: ['Sex7'],
-      },
-    };
+  // Testes para o método createInstructor
+  describe('createInstructor', () => {
+    it('should return "Missing fields" error if some value is empty', async () => {
+      const req = {
+        body: {
+          email: 'vinicius2@hotmail.com',
+          password: 'secret',
+          expertise: ['Matéria3'],
+          availability: ['Sex7'],
+        },
+      };
 
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    } as unknown as Response;
+      await createInstructor(
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
 
-    const next = jest.fn();
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({ msg: 'Missing fields' });
+    });
 
-    await createInstructor(
-      req as Request,
-      res as Response,
-      next as NextFunction
-    );
+    it('should return "Email already exists" error if email is already in use', async () => {
+      const req = {
+        body: {
+          name: 'Vinicius',
+          email: 'vinicius2@hotmail.com',
+          password: 'secret',
+          expertise: ['Matéria3'],
+          availability: ['Sex7'],
+        },
+      };
 
-    expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.json).toHaveBeenCalledWith({ msg: 'Missing fields' });
+      // Simula que um instructor com o email fornecido já existe
+      (InstructorModel.findOne as jest.Mock).mockResolvedValue({
+        email: 'viniciuspinha2@hotmail.com',
+      });
+
+      await createInstructor(
+        req as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({ msg: 'Email already exists' });
+    });
+  });
+
+  // Testes para o método updateInstructor
+  describe('updateInstructor', () => {
+    it('should return "The user ID is incorrect" error if the id is not a valid ObjectId', async () => {
+      const req = {
+        params: {
+          id: '123',
+        },
+      };
+
+      await updateInstructor(
+        req as any as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        msg: 'The user ID is incorrect',
+      });
+    });
+
+    it('should return "There is no instructor with id: {id}" error if there is no instructor with the given id', async () => {
+      const req = {
+        params: {
+          id: '6501ebfc87d46e3a6861844d',
+        },
+      };
+
+      // Simula que não há nenhum instructor com o id fornecido
+      (InstructorModel.findOne as jest.Mock).mockResolvedValue(null);
+
+      await updateInstructor(
+        req as any as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({
+        msg: `There is no instructor with id: ${req.params.id}`,
+      });
+    });
+  });
+
+  // Testes para o método deleteInstructor
+  describe('deleteInstructor', () => {
+    it('should return "There is no instructor with id: {id}" error if there is no instructor with the given id', async () => {
+      const req = {
+        params: {
+          id: '6501ebfc87d46e3a6861844d',
+        },
+      };
+
+      // Simula que não há nenhum instructor com o id fornecido
+      (InstructorModel.findOne as jest.Mock).mockResolvedValue(null);
+
+      await deleteInstructor(
+        req as any as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({
+        msg: `There is no instructor with id: ${req.params.id}`,
+      });
+    });
   });
 });
