aa2fbcc6221f84d31e4cbd6a213e64620735457e
Vinicius Francisco Pinha <viniciuspinha@alunos.utfpr.edu.br>
2023-09-14 11:08:40 -0300

    test: getSingleStudent test added

--- src/tests/student.test.ts
+++ src/tests/student.test.ts
@@ -15,6 +15,7 @@ jest.mock('../models/student', () => ({
     find: jest.fn(),
     findOneAndUpdate: jest.fn(),
     findByIdAndRemove: jest.fn(),
+    findById: jest.fn(),
   },
 }));
 
@@ -65,7 +66,7 @@ describe('Students tests', () => {
     });
 
     it('should return "There is none student registered" error if there is no student registered', async () => {
-      // Simula que não há nenhum instructor registrado
+      // Simula que não há nenhum student registrado
       (StudentModel.find as jest.Mock).mockResolvedValue([]);
 
       await getAllStudents(
@@ -77,6 +78,70 @@ describe('Students tests', () => {
       expect(res.status).toHaveBeenCalledWith(400);
       expect(res.json).toHaveBeenCalledWith({
         msg: 'There is none student registered',
+      });
+    });
+  });
+
+  describe('getSingleStudent', () => {
+    it('should return a student registred and "status OK"', async () => {
+      const req = {
+        params: {
+          id: '6501ebfc87d46e3a6861844d',
+        },
+      };
+
+      // Simula que há um student com o id fornecido
+      (StudentModel.findOne as jest.Mock).mockResolvedValue(mockStudent);
+
+      await getSingleStudent(
+        req as any as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(200);
+
+      expect(res.json).toHaveBeenCalledWith({ Student: mockStudent });
+    });
+
+    it('should return "The Student ID is incorrect" error if the id is not a valid ObjectId', async () => {
+      const req = {
+        params: {
+          id: '6501ebfc87d46e3a6861844g',
+        },
+      };
+
+      await getSingleStudent(
+        req as any as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.json).toHaveBeenCalledWith({
+        msg: 'The student ID is incorrect',
+      });
+    });
+
+    it('should return "There is no student with id: {id}" error if there is no Student with the given id', async () => {
+      const req = {
+        params: {
+          id: '6501ebfc87d46e3a6861844d',
+        },
+      };
+
+      // Simula que não há nenhum Student com o id fornecido
+      (StudentModel.findOne as jest.Mock).mockResolvedValue(null);
+
+      await getSingleStudent(
+        req as any as Request,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({
+        msg: `There is no student with id: ${req.params.id}`,
       });
     });
   });
