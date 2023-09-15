620f4da5d66f0e289137d81e9475e2593f7b163b
saulo <78312710+Saulo217@users.noreply.github.com>
2023-09-15 17:48:51 -0300

    fix: added route /schedules

--- src/routes/agendamento.ts
+++ src/routes/agendamento.ts
@@ -3,11 +3,13 @@ import {
   createSchedule,
   updateSchedule,
   deleteSchedule,
+  getAllSchedules,
 } from "../controllers/agendamento";
 import { verifyRoles, authMiddleware } from "../middleware/auth";
 
 const router = Router();
 
+router.route("/schedules").get(getAllSchedules);
 router
   .post("/schedule", authMiddleware, verifyRoles("Student"), createSchedule)
   .put("/schedule/:id", authMiddleware, verifyRoles("Student"), updateSchedule)
