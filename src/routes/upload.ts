ecb56e53d0fd63993f26bd596bf7e36a7df48215
Ana Leticia Vieira <leticiareisvr2@gmail.com>
2023-09-21 13:48:15 -0300

    feat: add removeUpload route

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,11 +1,15 @@
 import express from "express";
 const router = express.Router();
 
+<<<<<<< HEAD
 import {
   getScheduleUploads,
   uploadFile,
   removeUpload,
 } from "../controllers/upload";
+=======
+import { getScheduleUploads, uploadFile } from "../controllers/upload";
+>>>>>>> 2ded6d7f38201656224b6a6b46d6b2fca2df2e88
 import { authMiddleware, verifyUser } from "../middleware/auth";
 
 router
@@ -15,8 +19,11 @@ router
   .route("/schedule/:id/uploads")
   .get(authMiddleware, verifyUser, getScheduleUploads);
 
+<<<<<<< HEAD
 router
   .route("/schedule/:id/uploads/remove")
   .post(authMiddleware, verifyUser, removeUpload);
 
+=======
+>>>>>>> 2ded6d7f38201656224b6a6b46d6b2fca2df2e88
 export default router;
